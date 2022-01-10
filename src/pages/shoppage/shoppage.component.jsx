9fff9de2fabf8cd9807b2ef86649382639aac6a8
FernandoTorresDev <fernandotorrescandeia@gmail.com>
2022-01-10 19:13:21 -0300

    added header, added shoppage, styling, svg image

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import Collection from "../../components/collection/collection.component";
+import CollectionPreview from "../../components/collection/collection.component.jsx";
 
-import "./shoppage.styles.scss";
+import SHOP_DATA from "./shop.data.js";
 
-const ShopPage = () => (
-  <Collection />
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
