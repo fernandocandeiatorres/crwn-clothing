e1ffbe69c286a5f8e4c72977c385ef81dfaff65a
FernandoTorresDev <fernandotorrescandeia@gmail.com>
2022-01-10 11:47:42 -0300

    added by myself shopPage, collection component, and routing for those, and styling for collection

---
+++ src/pages/shoppage/shoppage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Collection from "../../components/collection/collection.component";
+
+import "./shoppage.styles.scss";
+
+const ShopPage = () => (
+  <Collection />
+
+);
+
+export default ShopPage;
